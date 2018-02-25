aa18ab7fd9306ce13b7eb21a9d286c96f78250f2
coderxiao2015 <2416160106@qq.com>
2018-02-25 22:48:17 +0800

    Initial commit

---
+++ src/com/ssm/xiao/controller/login/LoginAction.java
@@ -0,0 +1,13 @@
+package com.ssm.xiao.controller.login;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2018/2/25.
+ */
+@Controller
+@Scope(value = "prototype")
+public class LoginAction {
+
+}
---
+++ src/com/ssm/xiao/service/login/LoginService.java
@@ -0,0 +1,10 @@
+package com.ssm.xiao.service.login;
+
+import java.util.HashMap;
+
+/**
+ * Created by Administrator on 2018/2/25.
+ */
+public interface LoginService {
+    public int findUserInfo(HashMap<String,Object> param);
+}
